34e720ad60fed21aa1e4e6fbd691f04dc50cf931
shelleyyyyy <shelleywr23@mail.vmi.edu>
2023-01-30 11:00:36 -0500

    msg

---
+++ src/routes/coleismegadumb/+page.svelte
@@ -0,0 +1,13 @@
+<body class="bg-gray-200">
+    <div class="flex justify-center items-center h-screen">
+      <div class="w-3/4 p-6 bg-white rounded-lg shadow-xl">
+        <h1 class="text-2xl font-bold text-gray-900">Cole - The Foolish Young Man</h1>
+        <p class="text-gray-700 text-lg leading-normal mt-4">Once upon a time, there was a young man named Cole. Unlike other villagers, Cole was hated by all due to his foolish and careless behavior.</p>
+        <p class="text-gray-700 text-lg leading-normal mt-4">Despite being an adult, Cole acted like a child and often made poor decisions that caused problems for himself and others. For example, he would run around in the rain with his shoes untied, which would inevitably lead to him tripping and falling. This would cause inconvenience and annoyance to those around him.</p>
+        <p class="text-gray-700 text-lg leading-normal mt-4">One day, Cole decided to bake a cake for his friend's birthday. However, he accidentally added salt instead of sugar to the mixture, which resulted in an inedible and ruined cake. This mistake not only ruined the birthday celebration, but also made Cole's friend angry and frustrated.</p>
+        <p class="text-gray-700 text-lg leading-normal mt-4">On another occasion, Cole was helping a neighbor with their garden. However, he dug up all of their flowers and plants because he thought they were weeds. This careless act made the neighbor angry and resulted in a loss of their hard work and effort.</p>
+        <p class="text-gray-700 text-lg leading-normal mt-4">Due to his constant foolish behavior, Cole was hated by all in the town. People would avoid him and his presence would cause annoyance and frustration. Despite the consequences of his actions, Cole never learned from his mistakes and remained blissfully unaware.</p>
+        <p class="text-gray-700 text-lg leading-normal mt-4">In the end, Cole's foolishness not only caused problems for those around him, but also resulted in him being hated by all. He lived a lonely life, without the love and support of his community.</p>
+      </div>
+    </div>
+  </body>
